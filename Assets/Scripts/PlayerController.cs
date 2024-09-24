74f007f1a8aebd9d010bf016869f556f77464077
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2024-09-23 22:32:21 -0500

    fix: change main player script and reduce bullet life time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float _speed = 5f;
